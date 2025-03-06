630b7c151311164b8093a54dd1e8d9b45855d7c4
raissa <uefsraissa@gmail.com>
2025-03-05 22:07:29 -0300

    first commmit

---
+++ secao-2-variaveis-e-tipos-de-valores/exemplo-2.js
@@ -0,0 +1,9 @@
+let num1 = prompt("Digite o primeiro número: ");
+let num2 = prompt("Digite o segundo número: ");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+let soma = num1 + num2;
+
+alert(`A soma de ${num1} e ${num2} é ${soma}.`);
+
